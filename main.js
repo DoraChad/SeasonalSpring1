599d03bad5253e82c473543b974ea0011c9c9b10
DoraChad <176503791+DoraChad@users.noreply.github.com>
2026-03-11 08:38:25 -0700

    update 1

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
 const proxyURL = ["https://polyproxy.orangywastaken.workers.dev/leaderboard?version=0.6.0&trackId=", "&skip=0&amount=200&onlyVerified=false"]
 const trackIds = ["76e1920a3ca015033a0b21156848def2c248c95d97ccf4aab2312a0302beefe0", "a8132c5e2df877f86572476a14b24fedc5da0892d3136b1e8f0fad33013e829a", "4058e3616fbd79b848e70037adde4f12b4413011050aaf1c9d875cdbe2e33d68", "e65c13f972d370cae9c61c5c7dd53708c9328377b7db32000ebefeb64c9687d6", "dd7b5489ba2dc8691e713d7da5e25ea631d96183a7b9919556122898badda291", "3aa3612c79907e98105d9930b28172d25df7a0930ccaf0c3f096eb4d8e42400c", "064b75893da97ced0c44841f4ef2197c4c4e8c70fe75006e7fa538dbf37feccb", "c033a1a0805db87e0f040a12af1c387dda9b86611274fc82a88d7768ac168ef3", "aa421a6e2097e73cd34c3f580a6a68793aa927c7ade5520601879c6fb25b3b4e", "8ba04773833d77c33733fad05fdfc88b238bafe6013e1377238054a19ceab7bc`"]
-const numberOfTracks = 10
+const numberOfTracks = 16;
+const lockedTracks = 10;
+const trackData = {
+    names: [
+        "Star Bound",
+        "Chicken Itza",
+        "Amberbound",
+        "Fisherman's Dream",
+        "Sabakawa",
+        "Forgotten Act",
+        "Frozen Palaces",
+        "Devil's Factory",
+        "Pyx",
+        "Marble Valley"
+    ],
+    author: [
+        "By Skrdh & Zihcx",
+        "By qwertyuiop",
+        "By Raylee",
+        "By Ziggy",
+        "By DJPHILR",
+        "By HummusHere",
+        "By Blu3bolt",
+        "By Herny",
+        "By xav_au, hwadz & Hiksi",
+        "By Blu3bolt & Bruther"
+    ]
+};
+const calculationConstants = {
+    maxPoints: 100,
+}
 
 const saveVariableToFile = function(data, filename = "data.json") {
     const json = JSON.stringify(data, null, 2);
@@ -74,39 +104,60 @@ async function getFullLeaderboards(totalTracks = numberOfTracks) {
     }
 
     const playerData = {};
+    const WRs = {};
 
     leaderboards.forEach((lb, trackIndex) => {
         lb.entries.forEach((entry, pos) => {
             if (!playerData[entry.userId]) {
-            playerData[entry.userId] = {
-                nickname: entry.nickname,
-                placements: Array(totalTracks).fill(null)
-            };
+                playerData[entry.userId] = {
+                    nickname: entry.nickname,
+                    times: Array(totalTracks).fill(null),
+                    placements: Array(totalTracks).fill(null)
+                };
             }
+            playerData[entry.userId].times[trackIndex] = entry.frames;
             playerData[entry.userId].placements[trackIndex] = pos + 1;
+
+            if (pos === 0) {
+                WRs[trackIndex] = entry.frames;
+            }
         });
     });
 
-    return playerData;
+    return playerData, WRs;
 }
 
-function calculatePoints(playerMap) {
+function pointFormula(place, time, WR) {
+    let points;
+
+    if (place === 1) {
+        return calculationConstants.maxPoints * 1.1;
+    }
+
+    points = calculationConstants.maxPoints * ((1/((Math.E) ** (50/(Math.sqrt(WR))))) ** ((time - WR)/(WR)))
+
+    if (points <= 0) points = 0;
+
+    return points;
+}
+
+function calculatePoints(playerMap, WRs) {
     const playersWithPoints = [];
 
     for (const userId in playerMap) {
-    const player = playerMap[userId];
+        const player = playerMap[userId];
 
-    let points = 0;
-    player.placements.forEach(place => {
-        if (place === null) return;
-        points += 100 / Math.sqrt(place);
-    });
+        let points = 0;
+        player.placements.forEach((place, pos) => {
+            if (place === null) return;
+            points += pointFormula(place, player.times[pos], WRs[pos]);
+        });
 
-    playersWithPoints.push({
-        userId,
-        nickname: player.nickname,
-        points
-    });
+        playersWithPoints.push({
+            userId,
+            nickname: player.nickname,
+            points
+        });
     }
 
     playersWithPoints.sort((a, b) => b.points - a.points);
@@ -161,15 +212,44 @@ for (let i = 1; i <= numberOfTracks; i++) {
         doubleDiv.className = "double-div";
     }
 
+    const trackDiv = document.createElement("div");
+    trackDiv.className = "entry-div";
+
     const track = document.createElement("button");
-    track.addEventListener("click", () => {
-        copyFileToClipboard(i);
-    });
+
+    trackDiv.appendChild(track);
 
     track.className = "track";
-    track.style.backgroundImage = `url("https://raw.githubusercontent.com/DoraChad/SeasonalSpring1/refs/heads/main/images/WireFrames_MapTile_${i}.jpg")`;
+    if (i > lockedTracks) {
+        track.style.backgroundImage = `url("https://raw.githubusercontent.com/DoraChad/SeasonalSpring1/refs/heads/main/images/WireFrames_MapTile_Locked.jpg")`;
+    } else {
+        track.addEventListener("click", () => {
+            copyFileToClipboard(i);
+        });
+        track.style.backgroundImage = `url("https://raw.githubusercontent.com/DoraChad/SeasonalSpring1/refs/heads/main/images/WireFrames_MapTile_${i}.jpg")`;
 
-    doubleDiv.appendChild(track);
+        const title = document.createElement("p");
+        title.textContent = trackData["names"][i];
+        title.style.position = "absolute";
+        title.style.fontSize = "40px";
+        title.style.margin = "0";
+        title.style.left = "2%";
+        title.style.top = "2%";
+
+        trackDiv.appendChild(title);
+
+        const subTitle = document.createElement("p");
+        subTitle.textContent = trackData["author"][i];
+        subTitle.style.position = "absolute";
+        subTitle.style.fontSize = "20px";
+        subTitle.style.margin = "0";
+        subTitle.style.left = "2%";
+        subTitle.style.top = "12%";
+
+        trackDiv.appendChild(subTitle);
+    }
+
+    doubleDiv.appendChild(trackDiv);
 
     if (i % 2 === 0 || i === numberOfTracks) {
         tracksDiv.appendChild(doubleDiv);
